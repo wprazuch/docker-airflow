6799d10d5f66040a76214ea0d8f07dee9665e3fb
WojPrauch <wojciechprazuch3@gmail.com>
2020-11-15 11:16:55 +0100

    Removed for test

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -114,7 +114,6 @@ case "$1" in
       # With the "Local" and "Sequential" executors it should all run in one container.
       airflow scheduler &
     fi
-	airflow variables --import variables.json
     exec airflow webserver
     ;;
   worker|scheduler)
